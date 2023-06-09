1da2150aee2ee7f8f5e78af448465c44fce4bfae
duranysd <duranysd1119@gmail.com>
2023-06-09 16:21:15 -0600

    02 Variables

---
+++ variables.py
@@ -0,0 +1,14 @@
+#Python es no tipado
+name="Yahir"
+last_name="Duran"
+age=39
+height=1.68
+is_student=True
+group=["3 C"]
+
+#Function type
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(group))
