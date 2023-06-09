863194abce66f0972add71e522506c74699c90c7
duranysd <duranysd1119@gmail.com>
2023-06-09 16:52:33 -0600

    04 Operadores

---
+++ operadores.py
@@ -0,0 +1,21 @@
+#operadores matematicos
+#+ - * %
+#/Divicion float
+#//Divicion entera
+#**Potencia
+
+print(12+12)
+print(12-12)
+print(12*12)
+print(12%12)
+print(10/3)
+print(10//3)
+print(2** 16)
+
+#operadores para strings
+# + - para concatenar
+# * para replicar
+
+print("_____" * 4)
+print("Hola " + "Mundo")
+print("Hola " * 3)
