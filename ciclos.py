4b2a89487b5fb3ba94ca95bdc81b0091cab42281
duranysd <duranysd1119@gmail.com>
2023-06-20 12:54:00 -0600

    ciclos

---
+++ ciclos.py
@@ -0,0 +1,40 @@
+#ciclo while
+#while exp_bool: true
+
+i=0
+num=7
+while i <=10:
+    print(f"{num}*{i}={num * i}")
+    i +=1
+else:
+    print("ciclo terminado")
+
+#ciclo infinito
+while True:
+    #rompemos con break
+    break
+#el for recorre iterables
+#un iterable es algo que se puede recorrer
+
+#for variable in interable:
+my_string="hola"
+for letra in my_string:
+    print(letra, end=", ")
+
+lista=["uno", "dos", "tres", "cuatro"]
+for item in lista:
+    print(item)
+
+#fuction range
+#range (end)
+for i in range(10):
+    print(i, end=", ")
+print()
+#range(ini, end)
+for i in range(10, 20):
+    print(i, end=", ")
+print()
+#range(ini, end, step)
+for i in range(10, 20, 2):
+    print(i, end=", ")
+print()
